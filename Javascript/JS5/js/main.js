57de7d899128289b9ce7eb704db5e361e47554c0
AB027897 <47727880+AB027897@users.noreply.github.com>
2023-11-15 10:39:42 -0500

    Math Methods

---
+++ Javascript/JS5/js/main.js
@@ -0,0 +1,6 @@
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
