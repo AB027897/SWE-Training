36a9a42ab6bfccb4d86c5e32cca1b34275ca55cc
AB027897 <47727880+AB027897@users.noreply.github.com>
2023-11-15 11:11:31 -0500

    Switch Statements

--- Javascript/JS7/js/main.js
+++ Javascript/JS7/js/main.js
@@ -1,22 +1,29 @@
-
-if(playerOne === computer) {
-     
-}else if(playerOne === "rock") {
-    if(computer  == "paper") {
-        console.log("Computer wins");
-    } else {
-        console.log("Player One wins");
-    }
-} else if(playerOne === "paper"){
-    if(computer === "scissors") {
-        console.log("Computer wins");
-    } else {
-        console.log("Player One wins");
-    }
-} else {
-    if(computer === "rock") {
-        console.log("Computer wins");
-    } else {
-        console.log("Player One wins");
-    }
+let playerOne = "scissors";
+let computer = "rock";
+switch(playerOne) {
+    case computer:
+        console.log("Tie game!");
+        break;
+    case "rock":
+        if(computer === "paper") {
+            console.log("computer wins!");
+        } else {
+            console.log("playerOne wins!");
+        }
+        break;
+    case "paper":
+        if(computer === "scissors") {
+            console.log("computer wins!");
+        } else {
+            console.log("playerOne wins!");
+        }
+        break;
+    default:
+        if(computer === "rock") {
+            console.log("computer wins!");
+        } else {
+            console.log("playerOne wins!");
+        }
+        break; 
 }
+    
