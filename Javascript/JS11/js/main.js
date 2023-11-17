4000e18f5df18d6d7f4d2b7eda8e95cc460c513a
AB027897 <47727880+AB027897@users.noreply.github.com>
2023-11-16 21:24:57 -0500

    Refractor the Game with Arrays

--- Javascript/JS11/js/main.js
+++ Javascript/JS11/js/main.js
@@ -7,6 +7,7 @@ if(playGame) {
             let computerChoice = Math.floor(Math.random() * 3 +1);
             let computer = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors";
             let result = playerOne === computer ? "Tie game!" : playerOne === "rock" && computer === "paper" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!` : playerOne === "paper" && computer === "scissors" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!` : playerOne === "scissors" && computer === "rock" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!` : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+            alert(result);
             let playAgain = confirm("Play again?");
             playAgain ? location.reload() : alert("Ok, thanks for playing.");
         } else {
