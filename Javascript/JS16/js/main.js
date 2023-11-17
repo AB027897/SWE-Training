b8c9cfee1471503ec914d65ea5971795f8cdc70c
AB027897 <47727880+AB027897@users.noreply.github.com>
2023-11-16 22:27:16 -0500

    Objects

---
+++ Javascript/JS16/js/main.js
@@ -0,0 +1,13 @@
+const band = {
+    vocals: "Robert Plant",
+    guiter: "Jimmy Page",
+    bass: "John Paul Jones",
+    drums: "John Bonham"
+};
+
+
+function sings({vocals}) {
+    return `${vocals} sings!`
+}
+
+console.log(sings(band));
