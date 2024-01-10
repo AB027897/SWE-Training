482cca62e96d3524ea0c02332e4c44717fd8990b
AB027897 <amekness123@gmail.com>
2024-01-10 14:43:39 -0500

    Event Listeners

---
+++ Javascript/JS22/js/main.js
@@ -0,0 +1,16 @@
+//Syntax: addEventListener("click", function, useCapture)
+
+document.addEventListener("readystatechange", (event) => {
+    if(event.target.readyState == "complete") {
+        console.log("readyState: complete");
+        initApp();
+    }
+});
+const initApp = () => {
+   const view3 = document.querySelector("#view3");
+   const myForm = view3.querySelector("#myForm");
+   myForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("submit event");
+   });
+};
