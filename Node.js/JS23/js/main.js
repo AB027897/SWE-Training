3cb92dde0abf7c7bddcd6032ea9f372fe01308c1
AB027897 <amekness123@gmail.com>
2024-01-22 12:13:02 -0500

    Web Storage API

---
+++ Node.js/JS23/js/main.js
@@ -0,0 +1,13 @@
+const myArray = ['eat', 'sleep', 'code'];
+const myObject = {
+    name: "Dave",
+    hobbies: ['eat', 'sleep', 'code'],
+    logName: function () {
+        console.log(this.name);
+    }
+};
+
+localStorage.setItem("myLocalStore", JSON.stringify(myArray));
+const storeLength = localStorage.length;
+const myLocalData = JSON.parse(sessionStorage.getItem("myLocalStore"));
+console.log(storeLength);
