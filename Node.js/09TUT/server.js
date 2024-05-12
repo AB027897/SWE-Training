0c3737cbf6e4a4129bf6b3eded32d79c2f64ef59
Jake Nut <amekness123@gmail.com>
2024-05-11 22:50:01 -0400

    MVC REST API

--- Node.js/09TUT/server.js
+++ Node.js/09TUT/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions')
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
@@ -10,18 +11,6 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 //Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
-const corsOptions = {
-    origin : (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
-
 app.use(cors(corsOptions));
 
 app.use(express.urlencoded({extended : false}));
@@ -29,10 +18,8 @@ app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
